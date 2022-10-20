5cbda2e1160d7343e1ce2970ec5699f2fffe6e11
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-20 00:06:30 -0400

    Created basic script roadmap

---
+++ assets/scripts/index-script.js
@@ -0,0 +1,3 @@
+// First function will be to autogenerate project html based on my github repos. 
+// This will require some flags/tags/statuses to ensure the content is added correctly.
+// Additionally, this will require the use of the GitHub API. Sounds fun.
